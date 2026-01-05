d161036aedafc0e16898f54a76de88387c90cadc
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-05 17:44:48 +0100

    update

--- src/main/java/com/example/ecommerce/auth/AuthenticationController.java
+++ src/main/java/com/example/ecommerce/auth/AuthenticationController.java
@@ -1,4 +1,11 @@
 package com.example.ecommerce.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 }
---
+++ src/main/java/com/example/ecommerce/auth/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+}
