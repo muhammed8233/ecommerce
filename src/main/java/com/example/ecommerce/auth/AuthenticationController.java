7bb70ac9073c7aa5443e47b2aaec3e8da395d2f3
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-06 13:55:37 +0100

    update

--- src/main/java/com/example/ecommerce/auth/AuthenticationController.java
+++ src/main/java/com/example/ecommerce/auth/AuthenticationController.java
@@ -19,6 +19,12 @@ public class AuthenticationController {
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request){
-        return ResponseEntity.ok(authenticationService.rigister(request));
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 }
--- src/main/java/com/example/ecommerce/auth/AuthenticationService.java
+++ src/main/java/com/example/ecommerce/auth/AuthenticationService.java
@@ -1,9 +1,58 @@
 package com.example.ecommerce.auth;
 
-import org.springframework.http.ProblemDetail;
+import com.example.ecommerce.service.JwtService;
+import com.example.ecommerce.user.Role;
+import com.example.ecommerce.user.User;
+import com.example.ecommerce.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-    public ProblemDetail rigister(RegisterRequest request) {
-        return null;
+
+    @Autowired
+    private  JwtService jwtService;
+    @Autowired
+    private  PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User
+                .builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public @Nullable AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword())
+        );
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 }
