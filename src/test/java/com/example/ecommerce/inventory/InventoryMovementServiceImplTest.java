6017b238160adac51a073b1095a3bc5228d48e0b
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-08 17:53:49 +0100

    update

--- src/test/java/com/example/ecommerce/inventory/InventoryMovementServiceImplTest.java
+++ src/test/java/com/example/ecommerce/inventory/InventoryMovementServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.inventory;
 
+import com.example.ecommerce.product.Product;
 import com.example.ecommerce.product.ProductRequest;
 import com.example.ecommerce.product.ProductResponse;
 import com.example.ecommerce.product.ProductService;
@@ -8,6 +9,10 @@ import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -26,21 +31,29 @@ class InventoryMovementServiceImplTest {
     @Autowired
     private InventoryMovementRepository inventoryMovementRepository;
 
-//    @Test
-//    void restockProduct() {
-//        ProductRequest productRequest = ProductRequest.builder()
-//                .productName("bread")
-//                .category("medium")
-//                .description("family size")
-//                .price(new BigDecimal("2000"))
-//                .sku("BHM")
-//                .stockQuantity(1)
-//                .build();
-//       productService.createProduct(productRequest);
-//       List<ProductResponse> saved = productService.listProduct();
-//      assertEquals(0,inventoryMovementRepository.findAll().size());
-//      inventoryMovementService.restockProduct(saved.getFirst().getProductId(), 20);
-//        assertEquals(1,inventoryMovementRepository.findAll().size());
-//
-//    }
+    @Test
+    void restockProduct() {
+        ProductRequest productRequest = ProductRequest.builder()
+                .productName("bread")
+                .category("medium")
+                .description("family size")
+                .price(new BigDecimal("2000"))
+                .sku("BHM")
+                .stockQuantity(1)
+                .build();
+       productService.createProduct(productRequest);
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("price").ascending());
+       Page<ProductResponse> productPage = productService.getProducts("bread",pageable);
+
+        ProductResponse saved = productPage.getContent().get(0);
+      assertEquals(0,inventoryMovementRepository.findAll().size());
+      inventoryMovementService.restockProduct(saved.getProductId(), 20);
+        assertEquals(1,inventoryMovementRepository.findAll().size());
+
+
+        Product updatedProduct = productService.findProductById(saved.getProductId());
+        assertEquals(21, updatedProduct.getStockQuantity());
+
+    }
 }
